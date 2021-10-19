03ba1c9481d1681bb93a277429e4e807bf421b67
Felix W <WeigertF190039@sus.htl-grieskirchen.com>
2021-10-19 10:47:30 +0200

    viertes

--- src/pkg2hue/NumberTester.java
+++ src/pkg2hue/NumberTester.java
@@ -15,59 +15,98 @@ import java.util.logging.Logger;
  *
  * @author felix
  */
-public class NumberTester implements NumberTest{
-    
+public class NumberTester implements NumberTest {
+
     NumberTest oddTester;
     NumberTest primeTester;
     NumberTest palindromeTester;
-    
+
     String filename;
-    
-    public NumberTester(String fileName)
-    {
+
+    public NumberTester(String fileName) {
         this.filename = fileName;
-  
+
     }
-    
-    public void setOddEvenTester(NumberTest oddTester)
-    {
+
+    public void setOddEvenTester(NumberTest oddTester) {
         this.oddTester = oddTester;
     }
-    
-    public void setPrimeTester(NumberTest primeTester)
-    {
+
+    public void setPrimeTester(NumberTest primeTester) {
         this.primeTester = primeTester;
     }
-    
-    public void setPalindromeTester(NumberTest palindromeTester)
-    {
-     this.palindromeTester = palindromeTester;   
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
     }
-    
-    public void testFile()
-    {
+
+    public void testFile() {
+
+        oddTester = (number) -> {
+            if (number % 2 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+        primeTester = (number) -> {
+
+            if (number <= 1) {
+                return false;
+            }
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+
+        };
+
+        palindromeTester = (number) -> {
+
+            int palindrome = number;
+            int reverse = 0;
+
+            while (palindrome != 0) {
+                int remainder = palindrome % 10;
+                reverse = reverse * 10 + remainder;
+                palindrome = palindrome / 10;
+            }
+            if (number == reverse) {
+                return true;
+            }
+            return false;
+        };
+
         try {
             Scanner sc = new Scanner(new File(filename));
             int testfälle = Integer.parseInt(sc.nextLine());
             System.out.println("Testfälle: " + testfälle);
-            
-            while(sc.hasNext())
-            {
+
+            while (sc.hasNext()) {
                 String line = sc.nextLine();
                 String[] arr = line.split("\\s");
-                
+
+                int wassollgetestetwerden = Integer.parseInt(arr[0]);
+                int zahlzumtesten = Integer.parseInt(arr[1]);
+
+                switch (wassollgetestetwerden) {
+
+                }
+
             }
-            
+
         } catch (FileNotFoundException ex) {
             System.out.println("file not found");
         }
-        
-        
+
     }
 
     @Override
     public boolean testNumber(int number) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
